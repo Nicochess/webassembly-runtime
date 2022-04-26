11c1e7867d550e78f08d50bfe5ea23947f699602
Nicolas Magno <nicolasmagno.100@gmail.com>
2022-04-25 22:24:25 -0300

    fix: cleaning project to start e-commerce

--- src/App.js
+++ src/App.js
@@ -1,38 +1,4 @@
 import "./App.css";
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
-
-const initialState = {
-  name: "Nicolas",
-  id: 0,
-};
-
-const TESTE = "teste";
-
-const teste = (state = initialState, action) => {
-  return {
-    ...state,
-    teste: action.payload,
-  };
-};
-
-const contador = (state = initialState, action) => {
-  switch (action.type) {
-    case "INCREMENTAR":
-      return state.tot + 1;
-    default:
-      break;
-  }
-};
-
-const reducer = combineReducers({ contador, teste });
-
-const store = configureStore({
-  reducer: reducer,
-});
-
-store.dispatch({ type: TESTE, payload: "Olá redux" });
-
-console.log(store.getState());
 
 function App() {
   return <div className="App"></div>;
