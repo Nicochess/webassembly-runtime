8865fa0d810492d189bb9cb2f2f44877bc8b6241
Nicolas Magno <nicolasmagno.100@gmail.com>
2022-05-01 22:18:34 -0300

    feat: redux store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import "./App.css";
+import { configureStore } from "@reduxjs/toolkit";
+
+const reducer = () => {
+  return {
+    nome: "Nicolas",
+    id: 1,
+  };
+};
+
+const store = configureStore(reducer);
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
